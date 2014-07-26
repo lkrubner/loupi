a954a6cb064b444e47808345332cdf55b20b9dc8
Lawrence Krubner <lawrence@krubner.com>
2014-07-26 18:24:50 -0400

    Switching to the at-at library so we can use ScheduledThreadPoolExecutor to managea thread pool that correctly schedules repeating tasks.

--- src/clojure/loupi/perpetual.clj
+++ src/clojure/loupi/perpetual.clj
@@ -1,31 +1,30 @@
 (ns loupi.perpetual
-  (:import
-   (java.util TimerTask Timer))
   (:require
    [loupi.remember :as remember]
    [loupi.monitoring :as monitor]
    [loupi.dates :as dates]
+   [overtone.at-at :as at]
    [clojure.pprint :as pp]))
 
 
+
+(def my-pool (at/mk-pool))
+
 (defn resource-usage []
-  (let [task (proxy [TimerTask] [] (run []
-                                     (do
-                                       (println "The time is: " (dates/current-time-as-string))
-                                       (doseq [x (monitor/thread-top)]
-                                         (pp/pprint x))
-                                       (println (str (monitor/show-stats-regarding-resources-used-by-this-app))))))]
-    (. (new Timer) (schedule task (long 311000)))))
+  (at/every 311000
+            (fn []
+              (println "The time is: " (dates/current-time-as-string))
+              (doseq [x (monitor/thread-top)]
+                (pp/pprint x))
+              (println (str (monitor/show-stats-regarding-resources-used-by-this-app)))) my-pool))
 
 (defn show-whole-memory-cache []
-  (let [task (proxy [TimerTask] [] (run []
-                                     (do
-                                       (println "The time is: " (dates/current-time-as-string))
-                                       (println "We will now list everything in the memory cache:")
-                                       (doseq [x (remember/get-whole-memory-cache)]
-                                         (pp/pprint x)))))]
-    (. (new Timer) (schedule task (long 120000)))))
-
+  (at/every 120000
+            (fn []
+              (println "The time is: " (dates/current-time-as-string))
+              (println "We will now list everything in the memory cache:")
+              (doseq [x (remember/get-whole-memory-cache)]
+                (pp/pprint x))) my-pool))
 
 
 
