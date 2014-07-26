30a1d6f59f9b62a9144dc6799916e9a41e31ffd2
Lawrence Krubner <lawrence@krubner.com>
2014-07-26 14:24:01 -0400

    Using a timer so that repetitive tasks don't lead to StackOverflow errors.

--- src/clojure/loupi/perpetual.clj
+++ src/clojure/loupi/perpetual.clj
@@ -1,4 +1,6 @@
 (ns loupi.perpetual
+  (:import
+   (java.util TimerTask Timer))
   (:require
    [loupi.remember :as remember]
    [loupi.monitoring :as monitor]
@@ -6,27 +8,23 @@
    [clojure.pprint :as pp]))
 
 
-
 (defn resource-usage []
-  ;; 2013-05-28 - THIS FUNCTION IS CALLED AT STARTUP AND RUNS IN ITS OWN THREAD, REPEATING ENDLESSLY.
-  ;;
-  (println "The time is: " (dates/current-time-as-string))
-  (doseq [x (monitor/thread-top)]
-    (pp/pprint x))
-  (println (str (monitor/show-stats-regarding-resources-used-by-this-app)))
-  (. java.lang.Thread sleep 120000)
-  (resource-usage))
+  (let [task (proxy [TimerTask] [] (run []
+                                     (do
+                                       (println "The time is: " (dates/current-time-as-string))
+                                       (doseq [x (monitor/thread-top)]
+                                         (pp/pprint x))
+                                       (println (str (monitor/show-stats-regarding-resources-used-by-this-app))))))]
+    (. (new Timer) (schedule task (long 311000)))))
 
 (defn show-whole-memory-cache []
-  ;; 2013-05-28 - THIS FUNCTION IS CALLED AT STARTUP AND RUNS IN ITS OWN THREAD, REPEATING ENDLESSLY.
-  ;;
-  (println "The time is: " (dates/current-time-as-string))
-  (println "We will now list everything in the memory cache:")
-  (doseq [x (remember/get-whole-memory-cache)]
-    (pp/pprint @x))
-  (. java.lang.Thread sleep 120000)
-  (show-whole-memory-cache))
-
+  (let [task (proxy [TimerTask] [] (run []
+                                     (do
+                                       (println "The time is: " (dates/current-time-as-string))
+                                       (println "We will now list everything in the memory cache:")
+                                       (doseq [x (remember/get-whole-memory-cache)]
+                                         (pp/pprint x)))))]
+    (. (new Timer) (schedule task (long 120000)))))
 
 
 
